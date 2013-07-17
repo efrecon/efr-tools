f7c7de3c4f6127d011d51537d8ab82b6c6cd8a1f
emmanuel@sics.se <emmanuel@sics.se@1ce7f428-cadd-a38b-1752-b2d560808587>
2013-07-17 13:08:45 +0000

    Removed WS magic, not needed anymore

--- trunk/til/minihttpd/httpd.tcl
+++ trunk/til/minihttpd/httpd.tcl
@@ -37,7 +37,6 @@ namespace eval ::minihttpd {
 	    servers          ""
 	    dateformat       "\[%d%m%y %H:%M:%S\]"
 	    validate_timeout 250
-	    ws_magic         "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 	    -default         "index.htm index.html"
 	    -dirlist         "*"
 	    -logfile         ""
