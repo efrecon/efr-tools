c5d833d66476b8d6ef65290a979cbf4490bbb802
emmanuel@sics.se <emmanuel@sics.se@1ce7f428-cadd-a38b-1752-b2d560808587>
2013-08-28 12:19:40 +0000

    Context manager now re-read state from content of CSV files during startup phase

--- trunk/apps/cxManager/lib/csvout/csvout.tcl
+++ trunk/apps/cxManager/lib/csvout/csvout.tcl
@@ -38,6 +38,10 @@ namespace eval ::csvout {
 	    -cutter    "|"
 	    -format    "%Y%m%d-%H:%M:%S"
 	    -header    on
+	    -past      off
+	    -future    off
+	    -seeker    8192
+	    -jumper    0.9
 	}
 	variable version 0.1
 	variable libdir [file dirname [file normalize [info script]]]
@@ -128,6 +132,7 @@ proc ::csvout::__field { f val } {
 # Arguments:
 #	csvout	CSV context as created by new
 #	o	Identifier of object to open file for data output
+#	mode	Opening mode: dump for appending data, read for recap
 #
 # Results:
 #	Return a file descriptor ready for appending data, empty
@@ -135,7 +140,7 @@ proc ::csvout::__field { f val } {
 #
 # Side Effects:
 #	None.
-proc ::csvout::__open { csvout o } {
+proc ::csvout::__open { csvout o { mode "dump" } } {
     variable CSVOUT
     variable log
 
@@ -157,16 +162,28 @@ proc ::csvout::__open { csvout o } {
 	return ""
     }
 
-    # Open file for appending, testing its existence before opening so
-    # as to be able to automatically append a header at creation time.
+    # When in dumping mode, pen file for appending, testing its
+    # existence before opening so as to be able to automatically
+    # append a header at creation time.  When in reading mode only,
+    # simply open the file if it exists and return the file desciptor.
     set fname [file join $dir \
 		   [::uobj::resolve $o $COUT(-fname) [array get RESOLVER]]]
-    set header [expr ![file exists $fname]]
-    if { [catch {open $fname "a+"} fd] } {
-	${log}::warn "Cannot open file at $fname for CSV output: $fd"
-	set fd ""
+    if { $mode eq "dump" } {
+	set opener "a+"
     } else {
-	if { $header && [string is true $COUT(-header)] } {
+	set opener "r"
+    }
+    set header [expr ![file exists $fname]]
+    if { [catch {open $fname $opener} fd] } {
+	if { $mode eq "dump" } {
+	    # It's ok not to have any file to read when opening for
+	    # reading, since this is how we tell the caller that there
+	    # isn't anything to read right now.
+	    ${log}::warn "Cannot open file at $fname for CSV output: $fd"
+	}
+	set fd "";  # Nothing to read from or write to!
+    } else {
+	if { $mode eq "dump" && $header && [string is true $COUT(-header)] } {
 	    # Access the class directing the content of the object.
 	    set c [[$COUT(model) get schema] find [::uobj::type $o]]
 	    set fields [list __timestamp]
@@ -245,15 +262,37 @@ proc ::csvout::__flush { csvout o } {
     }
     upvar \#0 $csvout COUT
 
-    # Ignored scheduled writes, empty the output timer and the hints. 
-    if { [::uobj::keyword $o "when"] ne "" } {
-	::uobj::keyword $o csv/output ""
-	::uobj::keyword $o csv/hints ""
-	return {}
+    # Get the current time
+    set now [clock seconds]
+
+    # Only consider scheduled writes if we've been told so by the
+    # options.  Arrange in all cases for the variable <when> to
+    # contain the exact timestamp of the event to dump.
+    set when [::uobj::keyword $o "when"]
+    if { $when eq "" } {
+	set when $now
+    } else {
+	if { [string is true $COUT(-past)] && $when < $now } {
+	    # Let it pass happily, we'll have unordered and past
+	    # timestamps in the CSV file.
+	    ${log}::debug "Keeping past update"
+	} elseif { [string is true $COUT(-future)] && $when > $now } {
+	    # Let it pass happily, we'll have unordered and future
+	    # timestamps in the CSV file.
+	    ${log}::debug "Keeping future update"
+	} else {
+	    # Most of the time we want to stop when not dumping "live"
+	    # data, but rather data that will occur in the past or
+	    # future.
+	    ::uobj::keyword $o csv/output ""
+	    ::uobj::keyword $o csv/hints ""
+	    ${log}::debug "Discarding past or future update"
+	    return {}
+	}
     }
 
     # Open file for output, creating if necessary
-    set fd [__open $csvout $o]
+    set fd [__open $csvout $o "dump"]
 
     # Access the class directing the content of the object.
     set c [[$COUT(model) get schema] find [::uobj::type $o]]
@@ -266,11 +305,10 @@ proc ::csvout::__flush { csvout o } {
     set hints {}
     if { $c ne "" && $fd ne "" } {
 	upvar \#0 $o OBJ
-	set now [clock seconds]
 
 	# Prepend timestamp
 	set values [list [__quoted $csvout \
-			      [clock format $now -format $COUT(-format)]]]
+			      [clock format $when -format $COUT(-format)]]]
 	
 	# Continue with quoted content of all fields.
 	foreach s [$c inheritance on] {
@@ -282,7 +320,7 @@ proc ::csvout::__flush { csvout o } {
 	    }
 	}
 	
-	# Append an hint describing which fields were modified.
+	# Append a hint describing which fields were modified.
 	foreach h [::uobj::keyword $o csv/hints] {
 	    lappend hints [string trimleft $h "-"]
 	}
@@ -370,8 +408,79 @@ proc ::csvout::__write { csvout varname idx op } {
 #
 # Side Effects:
 #       Will arrange for content of the object updates to be written
-#       to the database that is associated to the object.
+#       to the CSV file that is associated to the object.
 proc ::csvout::__trace { csvout o } {
+    variable CSVOUT
+    variable log
+
+    if { ![::uobj::isa $csvout csvout] } {
+	return -code error "$csvout unkown or wrong type"
+    }
+    upvar \#0 $csvout COUT
+
+    if { ![followed $csvout $o] } {
+	trace add variable $o write [list [namespace current]::__write $csvout]
+	return $o
+    }
+
+    return ""
+}
+
+
+# ::csvout::follow -- Start following an object.
+#
+#       Arrange to bind an object to a CSV file: the state of the
+#       object will be read from the latest available state in the CSV
+#       file, then write traces will be created so as to record all
+#       present and future updates on the object.
+#
+# Arguments:
+#	csvout	CSV context, as created by <new>
+#       o       Identifier of the object
+#
+# Results:
+#       Return the identifier of the object if a trace was setup,
+#       empty string if no trace was installed, which means that we
+#       detected that there was already a trace from us on that
+#       object.
+#
+# Side Effects:
+#       Will arrange for content of the object updates to be written
+#       to the CSV file that is associated to the object.
+proc ::csvout::follow { csvout o } {
+    variable CSVOUT
+    variable log
+
+    if { ![::uobj::isa $csvout csvout] } {
+	return -code error "$csvout unkown or wrong type"
+    }
+    upvar \#0 $csvout COUT
+
+    if { ! [followed $csvout $o] } {
+	__recap $csvout $o
+	__trace $csvout $o
+
+	return $o
+    }
+    return ""
+}
+
+
+# ::csvout::followed -- Check if we are following an object
+#
+#       Check if this module is following a given object, detection is
+#       made through looking for matching write traces for the object.
+#
+# Arguments:
+#	csvout	CSV context, as created by <new>
+#       o       Identifier of the object
+#
+# Results:
+#       Return a boolean describing the following state of the object.
+#
+# Side Effects:
+#       None.
+proc ::csvout::followed { csvout o } {
     variable CSVOUT
     variable log
 
@@ -387,15 +496,317 @@ proc ::csvout::__trace { csvout o } {
 	    set found 1
 	}
     }
+    return $found
+}
 
-    # No trace was set up, install a write trace every time the
-    # object is written.
-    if { ! $found } {
-	trace add variable $o write [list [namespace current]::__write $csvout]
-	return $o
+
+# ::csvout::__split -- Split CSV line
+#
+#       Split a CSV line into the fields that it contains, paying
+#       proper attention to separators and quoting.  This code
+#       originates from the code available at the wiki:
+#       http://wiki.tcl.tk/2215 (scroll down!).  The current
+#       implementation probably does not work against lines that
+#       contain carriage returns or other end-of-line markers.
+#
+# Arguments:
+#	csvout	CSV context, as created by <new>
+#       line    Input line to split.
+#
+# Results:
+#       Return a list of all fields contained in the line, including
+#       empty fields.
+#
+# Side Effects:
+#       None.
+proc ::csvout::__split { csvout line } {
+    variable CSVOUT
+    variable log
+
+    upvar \#0 $csvout COUT
+
+    # Process each input character.
+    set commaRX [string map [list "%separator%" $COUT(-separator)] \
+		     {.*?(?=%separator%|$)}]
+    set result [list]
+    set beg 0
+    while {$beg < [string length $line]} {
+       if {[string index $line $beg] eq "\""} {
+          incr beg
+          set quote false
+          set word ""
+          foreach char [concat [split [string range $line $beg end] ""] \
+			    [list ""]] {
+             # Search forward for the closing quote, one character at a time.
+             incr beg
+             if {$quote} {
+		 if {[lsearch [list $COUT(-separator) ""] $char] >= 0} {
+                   # Quote followed by comma or end-of-line indicates the end of
+                   # the word.
+                   break
+                } elseif {$char eq $COUT(-quote)} {
+                   # Pair of quotes is valid.
+                   append word $char
+                } else {
+                   # No other characters can legally follow quote.  I think.
+                   return -code error \
+		       "extra characters after close-quote"
+                }
+                set quote false
+             } elseif {$char eq ""} {
+                # End-of-line inside quotes indicates embedded newline.
+                return -code error \
+		    "embedded newlines not supported"
+             } elseif {$char eq $COUT(-quote)} {
+                # Treat the next character specially.
+                set quote true
+             } else {
+                # All other characters pass through directly.
+                append word $char
+             }
+          }
+          lappend result $word
+       } else {
+          # Use all characters up to the comma or line ending.
+          regexp -start $beg $commaRX $line word
+          lappend result $word
+          set beg [expr {$beg + [string length $word] + 1}]
+       }
     }
 
-    return ""
+    # If the line ends in a comma, add a blank word to the result.
+    if {[string index $line end] eq $COUT(-separator)} {
+       lappend result ""
+    }
+
+    # Done.  Return the result list.
+    return $result    
+}
+
+
+# ::csvout::__update -- Update field in object
+#
+#       Update the content of a field in an object using a string
+#       value, as read from the CSV file.  This procedure will
+#       properly translate back from UUIDs to object identifier in the
+#       internal data space.
+#
+# Arguments:
+#	o	Identifier of object to update
+#       f       Identifier of field (from class)
+#	val	Value to write in field.
+#
+# Results:
+#       None.
+#
+# Side Effects:
+#       Change value of field in object.
+proc ::csvout::__update { o f val } {
+    variable CSVOUT
+    variable log
+    
+    upvar \#0 $o OBJ
+
+    if { [$f get builtin] || [$f get constraint] ne "" } {
+	set OBJ(-[$f get -name]) $val
+    } else {
+	if { [$f get -multi] } {
+	    set OBJ(-[$f get -name]) {}
+	    foreach v $val {
+		foreach {pv c} [::find:uuid $v object] break
+		if { $pv ne "" } {
+		    lappend OBJ(-[$f get -name]) $pv
+		}
+	    }
+	} else {
+	    foreach {v c} [::find:uuid $val object] break
+	    if { $v ne "" } {
+		set OBJ(-[$f get -name]) $v
+	    }
+	}
+    }
+}
+
+
+# ::csvout::__recap -- Revert to latest available state from CSV
+#
+#       Revert to the latest available state of the object, given
+#       content is available in the associated CSV file and the
+#       structure of the object has not changed.  This procedure is
+#       also able to take into account future timestamps if such had
+#       been used, i.e. it will get to the state that was wished at
+#       the time of the future update as long as this update appears
+#       at the end of the file.
+#
+# Arguments:
+#	csvout	CSV context, as created by <new>
+#	o	Identifier of object to update
+#
+# Results:
+#       Return the list of field names that were updated, an empty
+#       list in all other casese.
+#
+# Side Effects:
+#       Change value of fields in object according to CSV content.
+proc ::csvout::__recap { csvout o } {
+    variable CSVOUT
+    variable log
+
+    if { ![::uobj::isa $csvout csvout] } {
+	return -code error "$csvout unkown or wrong type"
+    }
+    upvar \#0 $csvout COUT
+
+    set updated {};   # List of fields updated by procedure
+
+    # Open CSV file, if any and find class for object.
+    set fd [__open $csvout $o "read"]
+    set c [[$COUT(model) get schema] find [::uobj::type $o]]
+
+    if { $fd eq "" || $c eq "" } {
+	${log}::debug "No history file or no class available for $o"
+    } else {
+	${log}::info "Reading old state for object $o from CSV history"
+
+	# Access all fields that are in the object, prepend the
+	# __timestamp and the __hints that are specific to the module
+	# and arrange for ofields to be that list and FMAPPER an array
+	# that will map from the name to the identifier of the fields.
+	array set FMAPPER {}
+	set ofields [list __timestamp]
+	upvar \#0 $o OBJ
+	    
+	# Continue with quoted content of all fields.
+	foreach s [$c inheritance on] {
+	    foreach f [$s get fields] {
+		lappend ofields [$f get -name]
+		set FMAPPER([$f get -name]) $f
+	    }
+	}
+	lappend ofields __hints
+
+	# Arrange for the variable fields to be the list of the fields
+	# that we will be finding in the CSV file.  We prefer reading
+	# from the header of the file, but can do with the fields
+	# currently in the object.  If we read from the file, scream
+	# if we have a mismatch, meaning we probably have an object
+	# that have changed description along its life.
+	set fields {}
+	if { [string is true $COUT(-header)] } {
+	    # Go to start of file and read header
+	    seek $fd 0
+	    set hdr [gets $fd]
+	    set fields [__split $csvout $hdr]
+	    
+	    # Now check file fields against current object and
+	    # vice-versa, screaming when we have a mismatch.
+	    foreach f $fields {
+		if { [lsearch $ofields $f] < 0 } {
+		    ${log}::critical \
+			"File field $f has been removed from object!"
+		    return {}
+		}
+	    }
+	    foreach f $ofields {
+		if { [lsearch $fields $f] < 0 } {
+		    ${log}::critical \
+			"Object field $f cannot be found in file!"
+		    return {}
+		}
+	    }
+	} else {
+	    # If we didn't have header in the file, trust the
+	    # object...
+	    set fields $ofields
+	}
+
+	# Go towards the end of the file in clever ways.  For small
+	# files, we can afford reading everything. For bigger files,
+	# jump to the end of the file to minimise CSV parsing overhead
+	# (at the risk of missing the real latest update, but this is
+	# unlikely by construction).
+	seek $fd 0 end
+	set size [tell $fd]
+	if { $size > $COUT(-seeker) } {
+	    # If the file is big, jump forward towards the end of it,
+	    # and read a junk line since we have no idea where we've
+	    # arrived.
+	    set jumper [expr int($COUT(-jumper)*$size)]
+	    if { $size - $jumper > $COUT(-seeker) } {
+		${log}::debug "File size 'enormous', skipping to\
+                               $COUT(-seeker) from its end."
+		seek $fd [expr -$COUT(-seeker)] end
+	    } else {
+		${log}::debug "File size greater than $COUT(-seeker) bytes,\
+                               skipping first $jumper bytes of file for quicker\
+                               access."
+		seek $fd $jumper start
+	    }
+	    gets $fd;  # Skip junk line, which ensures we'll never get
+		       # the header either.
+	} else {
+	    ${log}::debug "File size is $size bytes, parsing it all to look\
+                           for latest update"
+	    seek $fd 0 start; # Start from the very beginning of the file
+	    # skip header, we've already parsed that information anyway...
+	    if { [string is true $COUT(-header)] } {
+		set hdr [gets $fd]
+	    }
+	}
+
+	# Now read data stepwise and look for the one that is either
+	# the latest timestamp available, as long as it's not a future
+	# timestamp that would be placed ahead of current time.  This
+	# means that the present algorithm is able to actually recap
+	# "future" timestamps from CSV files in a number of cases.
+	set now [clock seconds]
+	set max 0
+	set latest {}
+	while { ![eof $fd] } {
+	    # Get line and split
+	    set line [string trim [gets $fd]]
+	    if { $line ne "" } {
+		set updt [__split $csvout $line]
+
+		# Construct DESCR array with field content of the line
+		for {set i 0} {$i<[llength $fields]} {incr i} {
+		    set DESCR([lindex $fields $i]) [lindex $updt $i]
+		}
+
+		if { [catch {clock scan $DESCR(__timestamp) \
+				 -format $COUT(-format)} when] } {
+		    ${log}::warn "Could not understand $DESCR(__timestamp) as\
+                                  a timestamp in format $COUT(-format)"
+		} else {
+		    if { $when <= $now && $when > $max } {
+			set latest $updt
+		    }
+		}
+	    }
+	}
+
+	# We have found the latest update for the object, update
+	# current object with latest value, performing the proper
+	# translations between how we wrote to the CSV file and how
+	# data is represented in memory space.
+	if { [llength $latest] > 0 } {
+	    for {set i 0} {$i<[llength $fields]} {incr i} {
+		set fname [lindex $fields $i]
+		if { $fname ne "__timestamp" && $fname ne "__hints" } {
+		    set value [lindex $latest $i]
+		    set f $FMAPPER($fname)
+		    if { [catch {__update $o $f $value} err] } {
+			${log}::warn "Could not update field $fname in $o to\
+                                      '$value': $err"
+		    } else {
+			lappend updated $fname
+		    }
+		}
+	    }
+	}
+    }
+
+    return $updated
 }
 
 
@@ -430,7 +841,7 @@ proc ::csvout::config { csvout args } {
     # on write and arrange for setting them up if they are missing.
     set traced [list]
     foreach o [$COUT(model) get objects] {
-	if { [__trace $csvout $o] ne "" } {
+	if { [follow $csvout $o] ne "" } {
 	    lappend traced $o
 	}
     }
@@ -479,7 +890,7 @@ proc ::csvout::new { mdl args } {
 	[list [namespace current]::__trace $csvout %i]
 
     ::uobj::inherit CSVOUT COUT
-    ::uobj::objectify $csvout [list [list config configure]]
+    ::uobj::objectify $csvout [list [list config configure] followed follow]
     
     eval config $csvout $args
 
